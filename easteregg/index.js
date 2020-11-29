29b94fefc98926b09d6b28afe176e47f21bc0c98
Bea <beatriztattooer@gmail.com>
2020-11-29 11:09:05 -0300

    EASTER EGG: projectiles It's shooting! Shooting everywhere!

--- easteregg/index.js
+++ easteregg/index.js
@@ -21,10 +21,68 @@ class Player {
         c.fill()
     }
 }
+class Projectile {
+    // to create shoots:
+    constructor(x, y, radius, color, velocity){
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+    }
+    // to show the shoots at screen:
+    draw(){
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+    }
+    // to draw the path of the shoot:
+    update(){
+        this.draw()
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+    }
+}
 
 const x = canvas.width/2
 const y = canvas.height/2
 const player = new Player(x, y, 30, 'blue')
 player.draw()
 
-console.log(player)
+// to store the projectiles:
+const projectiles = []
+
+function animate(){
+    requestAnimationFrame(animate)
+    c.clearRect(0, 0, canvas.width, canvas.height)
+    player.draw()
+    projectiles.forEach((projectile) => {
+        projectile.update()
+    })
+}
+
+addEventListener('click', (event) => {
+    const angle = Math.atan2(
+        event.clientY - canvas.height / 2,
+        event.clientX - canvas.width / 2
+        )
+    const velocity = {
+        x: Math.cos(angle),
+        y: Math.sin(angle)
+    }
+
+    console.log(velocity)
+
+    projectiles.push(
+        new Projectile(
+            canvas.width / 2,
+            canvas.height / 2,
+            '5',
+            'red',
+            velocity
+        )
+    )
+})
+
+animate()
